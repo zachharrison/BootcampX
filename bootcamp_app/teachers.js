f7c66b00f3eff4e92757eca281368f7f274ecbf6
Zach Harrison <zharrison100@gmail.com>
2021-01-07 04:11:47 +0000

    Refactored students.js and teachers.js to protect agains sql injection attacks

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
   SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
   FROM teachers
   JOIN assistance_requests ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
+  WHERE cohorts.name = $1
   ORDER BY teachers.name;
-`)
+`;
+
+const values = [process.argv[2]];
+
+pool
+.query(queryString, values)
 .then(res => {
   res.rows.forEach(assistance => console.log(`${assistance.cohort}: ${assistance.teacher}`))
 })
